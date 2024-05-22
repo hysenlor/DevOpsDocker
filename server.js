345c4d49e96578a9bda5f9339b8620ee60de7638
Loren Hyseni <hysenlor@students.zhaw.ch>
2024-05-22 23:17:14 +0200

    Server.js neuer Smile :D

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-	res.send('Hello FS2024 DevOps Course, greetings from Loren! :-))) (((-:');
+	res.send('Hello FS2024 DevOps Course, greetings from Loren! :D');
 });
 
 app.listen(PORT, HOST);
